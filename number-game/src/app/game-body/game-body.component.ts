6d2cd0204f1c3ea39100fd48a19241a5ac262aac
AbhinavSingirthi <singirthi.abhinav@gmail.com>
2021-02-15 22:24:15 +0530

    Added images and card-redirection code

--- number-game/src/app/game-body/game-body.component.ts
+++ number-game/src/app/game-body/game-body.component.ts
@@ -7,14 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameBodyComponent implements OnInit {
 
-  cardQuote: string = 'Everything around you is numbers';
-  cardImageUrl: string = 'assets/image_1.PNG';
   imageWidth: number = 10;
-  cardDescription: string = '~~~ Click Next to Start ~~~'
+  cardQuote: string = '';
+  cardImageUrl: string = '';
+  cardDescription: string = '';
+  cardCounter: number = 0;
+
+  cardArr: any = [
+    {
+      "cardId": 1,
+      "cardQuote": "Everything around you is numbers",
+      "cardImageUrl": "assets/image_1.PNG",
+      "cardDescription": "~~~ Click Next to Start ~~~"
+    },
+    {
+      "cardId": 2,
+      "cardQuote": "Take a TWO-DIGIT number in your mind!",
+      "cardImageUrl": "assets/image_2.jpg",
+      "cardDescription": "Example: 22"
+    },
+    {
+      "cardId": 3,
+      "cardQuote": "Now, Add the digits of your Number",
+      "cardImageUrl": "assets/image_3.jpg",
+      "cardDescription": "Example: 22 => 2 + 2 => 4"
+    },
+    {
+      "cardId": 4,
+      "cardQuote": "Now, Subtract the SUM from your TWO-DIGIT number",
+      "cardImageUrl": "assets/image_4.jpg",
+      "cardDescription": "Example: 22 - 4 => 18"
+    },
+    {
+      "cardId": 5,
+      "cardQuote": "Stop the generator and add that number to your result",
+      "cardImageUrl": "NOIMAGE",
+      "cardDescription": ""
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.cardCounter = 0;
+    this.cardQuote = this.cardArr[this.cardCounter].cardQuote;
+    this.cardImageUrl = this.cardArr[this.cardCounter].cardImageUrl;
+    this.cardDescription = this.cardArr[this.cardCounter].cardDescription;
   }
 
+  setCardDetails(counter: number): void{
+    this.cardQuote = this.cardArr[counter].cardQuote;
+    this.cardImageUrl = this.cardArr[counter].cardImageUrl;
+    this.cardDescription = this.cardArr[counter].cardDescription;
+  }
+
+  nextCard(): void{
+    if(this.cardCounter < 4){
+      this.cardCounter++;
+      this.setCardDetails(this.cardCounter);
+    }
+  }
+
+  goBack(): void{
+    if(this.cardCounter > 0){
+      this.cardCounter--;
+      this.setCardDetails(this.cardCounter);
+    }
+  }
 }
