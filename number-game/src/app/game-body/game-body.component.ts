ab7f5445f67c1eeef1f448697d02732d56a9e509
AbhinavSingirthi <singirthi.abhinav@gmail.com>
2021-02-15 20:47:02 +0530

    added header and footer

---
+++ number-game/src/app/game-body/game-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'math-game-body',
+  templateUrl: './game-body.component.html',
+  styleUrls: ['./game-body.component.css']
+})
+export class GameBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
